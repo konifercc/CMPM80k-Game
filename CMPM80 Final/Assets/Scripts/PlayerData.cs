d3648e2aeb29262c44a81b4a9ee7f1d0f593a8e7
nathanyu8th <nathanyu8th@gmail.com>
2024-11-12 00:30:45 -0800

    new scene

--- CMPM80 Final/Assets/Scripts/PlayerData.cs
+++ CMPM80 Final/Assets/Scripts/PlayerData.cs
@@ -60,7 +60,9 @@ public class PlayerData : ScriptableObject
     [Header("Assists")]
 	[Range(0.01f, 0.5f)] public float coyoteTime; //Grace period after falling off a platform, where you can still jump
 	[Range(0.01f, 0.5f)] public float jumpInputBufferTime; //Grace period after pressing jump where a jump will be automatically performed once the requirements (eg. being grounded) are met.
-	
+
+	[Header("Health")]
+	public float playerHealth;
 
 	//Unity Callback, called when the inspector updates
     private void OnValidate()
